77678e648bb3f95f4ec18df0b4192268103a57d1
Your Name <you@example.com>
2019-05-06 12:41:02 +0800

    modify ndarray

--- data_analyze/numpy_compute.py
+++ data_analyze/numpy_compute.py
@@ -4,6 +4,13 @@ import numpy as np
 # 第一种方式使用python列表创建一维数组
 x = np.array([1,2,3])
 print("一维数组: ", x)
+# 如果是一维，整个数组相加
+print(sum(x))
+
+x = np.array([[1,2,3], [4,5,6]])
+print("二维数组: \n", x)
+#如果是二维, 列相加，返回一维ndarray 
+print(sum(x))
 
 # 如果参数中是可选的话，你得加key设置value
 # x = np.random.randint(low=10, high=12, size=(3, 4))
@@ -54,7 +61,199 @@ print(x)
 x = np.arange(start=0, stop=10, step=2)
 print(x)
 
-# np.linspace
+# axis=轴对称
+x = np.linspace(2.0, 3.0, num=5)
+print(x)
+
+# axis=轴对称, 默认产生float，制定dtype
+x = np.linspace(2, 20, num=5, dtype='int32')
+print(x)
+
+# 一维数组转二维数组
+x = np.arange(9).reshape((3,3))
+print(x)
+
+# 创建随机的3行两列的ndarray, item 范围[0, 1]
+x = np.random.rand(3,2)
+print(x)
+# 创建随机的3行两列的ndarray, item 范围[0, 10]
+x = np.random.rand(3,2) * 10
+print(x)
+
+# 创建随机的3行3列的ndarray, item 范围[0, 10]
+x = np.random.randint(high=10 ,low=0,  size=(3,3))
+print(x)
+
+# 随机生成指定均值与标准差的ndarray
+x = np.random.normal(loc=0.0, scale=1.0, size=(4,4))
+print(x)
+#print(sum(x))
+# 均值
+print(x.mean())
+print(x.shape)
+# item 的数据类型
+print(x.dtype)
+print("x>0 个数为 ", (x>0).sum())
+print("x<0 个数为 ", (x<0).sum())
+
+x = np.random.randint(low=0,high=33, size=(4,4))
+print(x)
+
+# reshape 一维转二维, 创建连续二维数组
+x = np.arange(start=2, stop=34, step=2).reshape(4,4)
+print(x)
+print(len(x))
+# 元素个数
+print(x.size)
+
+
+x = np.linspace(2,32,16).reshape(4,4)
+print(x)
+
+
+#######################访问和删除 ndarray 中的元素及向其中插入元素##################
+print("#######################访问和删除 ndarray 中的元素及向其中插入元素##################")
+x = np.array([1, 2, 3, 4, 5])
+print("origin ndarray :\n", x)
+# 一维数组索引从正向
+print(x[0])
+# 索引从逆向
+print(x[-1])
+
+x[3] = 44444
+print("modify ndarray :\n", x)
+
+# 二维数组
+x = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(x)
+# row, column的索引
+print(x[2][1])
+
+#########修改元素
+x[2][1] = 99
+
+print(x)
+
+##########删除元素########
+# We create a rank 1 ndarray 
+x = np.array([1, 2, 3, 4, 5])
+leftArr = np.delete(x, [0,1], axis=0)
+print("leftArr: \n", leftArr)
+print(x)
+
+
+# We create a rank 2 ndarray
+y = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(y)
+# 删除第0行与第2行 axis=0表示横轴
+leftArr = np.delete(y, [0,2], axis=0)
+print("删除第0行与第2行 leftArr: \n", leftArr)
+print(y)
+
+# 删除第0列与第2列 axis=1表示纵轴
+leftArr = np.delete(y, [0,2], axis=1)
+print("删除第0列与第2列 leftArr: \n", leftArr)
+print(y)
+
+
+############添加元素##############
+# We create a rank 1 ndarray 
+x = np.array([1, 2, 3, 4, 5])
+
+# We create a rank 2 ndarray 
+y = np.array([[1,2,3],[4,5,6]])
+
+# We print x
+print()
+print('Original x = ', x)
+
+# append 添加到末尾
+left = np.append(x, [6,7], axis=0)
+print("一维数组添加之后 \n", left)
+print(x)
+
+
+left = np.append(y, [[7],[8]], axis=1)
+print("二维数组添加(必须是维度一致)之后 \n", left)
+print("原始的ndarray不会改变 y: \n" ,y)
+
+############insert插入元素##############
+left = np.insert(x, 0, [0, -1, -2], axis=0)
+print("插入一维数组元素x: \n", left)
+print(x)
+
+left = np.insert(y, 0, [0,88], axis=1)
+print("插入一列二维数组元素x: \n", left)
+print(x)
+
+left = np.insert(y, 0, [[0,88], [0,88]], axis=1)
+print("插入二维数组元素x: \n", left)
+
+# 插入的全部是0,只需要写一个
+left = np.insert(y, 0, [0], axis=1)
+print("插入二维数组元素x: \n", left)
+print(x)
+
+
+####################堆叠###########
+# We create a rank 1 ndarray 
+x = np.array([1,2])
+
+# We create a rank 2 ndarray 
+y = np.array([[3,4],[5,6]])
+
+# We print x
+print()
+print('x = ', x)
+
+# We print Y
+print()
+print('Y = \n', y)
+
+#垂直堆叠tup 元组 We stack x on top of Y
+result = np.vstack((x,y))
+print("垂直堆叠stack x on top of Y : \n", result)
+print("一维转二维 \n",x.reshape(2,1))
+result = np.hstack((x.reshape(2,1),y))
+print("横向堆叠 \n", result)
+
+a = np.array([1, 2, 3]) 
+b = np.array([2, 3, 4]) 
+result = np.vstack((a,b)) 
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
